5ed4d3b7ca7582e9c2409163427f659e47a4a177
John King <jking6884@yahoo.com>
2016-12-02 02:40:03 -0500

    tutorial-4 - set up permissions and security layers for the endpoints

--- tutorial/urls.py
+++ tutorial/urls.py
@@ -1,8 +1,12 @@
 from django.conf.urls import url, include
 from django.contrib import admin
-from django.conf.urls import url, include
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^', include('snippets.urls')),
+]
+
+urlpatterns += [
+    url(r'^api-auth/', include('rest_framework.urls',
+                               namespace='rest_framework')),
 ]
